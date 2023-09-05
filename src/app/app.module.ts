d39210a39ee3cf59cbb0eb568c3932e710c46af5
wvictoriadev <wvictoriadev@gmail.com>
2023-09-05 10:36:43 -0500

    :computer: Modularizando auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
+import { PagesModule } from './pages/pages.module';
+import { AuthModule } from './auth/auth.module';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
 import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { PagesComponent } from './pages/pages.component';
-import { CarsComponent } from './pages/cars/cars.component';
-import { EmployeesComponent } from './pages/employees/employees.component';
-import { TripsComponent } from './pages/trips/trips.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    PagenotfoundComponent,
-    DashboardComponent,
-    BreadcrumbsComponent,
-    SidebarComponent,
-    HeaderComponent,
-    PagesComponent,
-    CarsComponent,
-    EmployeesComponent,
-    TripsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, PagenotfoundComponent],
+  imports: [BrowserModule, AppRoutingModule, PagesModule, AuthModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
