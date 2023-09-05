71790d97292d9cc5c268ae17b16a1ceffc6cc731
wvictoriadev <wvictoriadev@gmail.com>
2023-09-05 11:29:39 -0500

    :computer: Implementacion formulario registro

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+  public registerForm = this.fb.group({
+    nombre: ['Juan Carlos', [Validators.required, Validators.minLength(3)]],
+    email: ['juan.velez@gmail.com', [Validators.required]],
+    password: ['12345678a', [Validators.required]],
+    password2: ['12345678a', [Validators.required]],
+    terminos: [false, [Validators.required]],
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
+  crearUsuario(){
+    console.log(this.registerForm.value);
   }
-
 }
--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
-  styleUrls: ['./pages.component.css']
 })
 export class PagesComponent implements OnInit {
 
