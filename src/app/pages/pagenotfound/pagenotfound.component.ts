f8b150e4adf94e087a8a0ad08402c24ee9499211
wvictoriadev <wvictoriadev@gmail.com>
2023-09-05 09:48:44 -0500

    :computer: Implementacion estilos pagina PageNotFound

--- src/app/pages/pagenotfound/pagenotfound.component.ts
+++ src/app/pages/pagenotfound/pagenotfound.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pagenotfound',
   templateUrl: './pagenotfound.component.html',
-  styleUrls: ['./pagenotfound.component.css']
+  styleUrls: ['./pagenotfound.component.css'],
 })
 export class PagenotfoundComponent implements OnInit {
+  year = new Date().getFullYear();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
