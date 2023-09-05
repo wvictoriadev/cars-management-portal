866504a1cd64e5bcb327ea6af738f79ca1614fd4
wvictoriadev <wvictoriadev@gmail.com>
2023-09-05 09:33:36 -0500

    :computer: Implementacion de rutas secundarias

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,25 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
+import { PagesComponent } from './pages/pages.component';
+import { CarsComponent } from './pages/cars/cars.component';
+import { EmployeesComponent } from './pages/employees/employees.component';
+import { TripsComponent } from './pages/trips/trips.component';
 
 const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'carros', component: CarsComponent },
+      { path: 'empleados', component: EmployeesComponent },
+      { path: 'viajes', component: TripsComponent },
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    ],
+  },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  // Ruta que no exista
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: '**', component: PagenotfoundComponent },
 ];
 
