d381c206cb158cb587d33459b1838b137c409570
wvictoriadev <wvictoriadev@gmail.com>
2023-09-05 08:16:12 -0500

    :tada: Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    PagenotfoundComponent,
+    DashboardComponent,
+    BreadcrumbsComponent,
+    SidebarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
